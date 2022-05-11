a35591502df05e1360e5bb8671656db98d62b947
Zak <74899902+ZakariaBouhou@users.noreply.github.com>
2022-05-11 14:16:30 +0200

    Update bdd.php

--- src/bdd.php
+++ src/bdd.php
@@ -2,7 +2,7 @@
 
 try {
 
-    $bdd = new PDO('mysql:host=localhost;dbname=netflix;charset=utf8', 'dbzak', 'Cuicui75012!');
+    $bdd = new PDO('mysql:host=localhost;dbname=netflix;charset=utf8', 'root', '');
 }
 
 catch(Exception $e) {
